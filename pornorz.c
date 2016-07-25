24c23b4a6296527339fdd3130c158ac2a37a2b61
prudhvirajstark <panisettiprudhviraj@gmail.com>
2016-07-25 17:24:04 +0530

    Add files via upload

---
+++ pornorz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+ if(a>0)
+ {
+  printf("%d",a%100);
+ }
+ else if(a<0)
+ {
+  printf("0");
+ }
+ else
+ {
+  printf("0");
+ }
+}
