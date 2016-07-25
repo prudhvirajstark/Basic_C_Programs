892b6de1b380a623a6d8033b52aa94e57d4d0f3b
prudhvirajstark <panisettiprudhviraj@gmail.com>
2016-07-25 20:43:17 +0530

    Add files via upload

---
+++ aorn.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ char ch;
+ scanf("%c",&ch);
+ if((ch>=65&&ch<==90)||(ch>=97&&ch<=122))
+printf("alphabet");
+ else
+ printf("Not an Alphabet");
+}
