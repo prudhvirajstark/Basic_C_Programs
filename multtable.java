fd9a576d6cfae0b8eba5cb5738ef5e1dd299b925
prudhvirajstark <panisettiprudhviraj@gmail.com>
2016-07-28 01:15:55 +0530

    Add files via upload

---
+++ multtable.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+    for(int i=1;i<=10;i++)
+        {
+        int s = N*i;
+        System.out.print(+N);
+    System.out.print(" x "+i);
+        System.out.print(" = "+s);
+        System.out.println();
+    }
+    }
+}
