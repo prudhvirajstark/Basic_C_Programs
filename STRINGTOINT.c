00aa76f784c6861241ee3c405c8a1957398f0ad1
prudhvirajstark <panisettiprudhviraj@gmail.com>
2016-08-30 12:59:05 +0530

    Add files via upload

---
+++ STRINGTOINT.c
@@ -0,0 +1,16 @@
+// PROGRAM TO CONVERT STRING TO INTEGER
+#include<stdio.h>
+#include<stdlib.h>
+ 
+int main() {
+   int num;
+   char marks[3];
+ 
+   printf("Please Enter Marks : ");
+   scanf("%s", marks);
+ 
+   num = atoi(marks);
+   printf("\nMarks : %d", num);
+ 
+   return (0);
+}
